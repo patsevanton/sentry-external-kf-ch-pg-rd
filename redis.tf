a98e9eddd9bba62556b432ee66af27db7e127f76
Anton Patsev <patcev_aa@magnit.ru>
2025-01-23 11:57:39 +0600

    refactor redis

--- redis.tf
+++ redis.tf
@@ -1,21 +1,23 @@
-module "redis" {
-  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-redis.git?ref=v1.0.0"
+resource "yandex_mdb_redis_cluster" "sentry" {
+  name        = "sentry"
+  folder_id   = local.folder_id
+  network_id  = yandex_vpc_network.sentry.id
+  environment = "PRODUCTION"
 
-  name       = "sentry"
-  folder_id  = local.folder_id
-  network_id = yandex_vpc_network.sentry.id
-  redis_version = 7.2
-
-  password         = "secretpassword"
-  maxmemory_policy = "ALLKEYS_LRU"
-
-  hosts = {
-    host1 = {
-      zone      = "ru-central1-a"
-      subnet_id = yandex_vpc_subnet.sentry-a.id
-    }
+  config {
+    password         = "secretpassword"
+    maxmemory_policy = "ALLKEYS_LRU"
+    version     = "7.2"
   }
 
-  zone = "ru-central1-a"
+  resources {
+    resource_preset_id = "hm1.nano"
+    disk_type_id       = "network-ssd"
+    disk_size          = 16
+  }
 
+  host {
+    zone      = "ru-central1-a"
+    subnet_id = yandex_vpc_subnet.sentry-a.id
+  }
 }
